df867fb57179147ceb0984b7fa3f6203633cccb2
cha62 <charles.parisi62@gmail.com>
2023-12-09 14:02:35 +0100

    premiere grille avec bug

--- file.py
+++ file.py
@@ -1,12 +1,4 @@
-import msvcrt
+word = "salut"
 
-def get_key():
-    key = msvcrt.getch()
-    if key == b'\r':
-        return "Enter"
-    elif key == b'\x08':  # Detection de la touche de suppression (Backspace)
-        return "Back"
-    else:
-        return key.decode()
-
-print(get_key())
+for l in word :
+    print(l)
--- letters.py
+++ letters.py
@@ -1,14 +1,12 @@
 import msvcrt
 
-
-
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def get_key():
     key = msvcrt.getch()
-    if key == b'\r':
+    if key == b'\r': # Enter
         return "Enter"
-    elif key == b'\x08':  # Detection de la touche de suppression (Backspace)
+    elif key == b'\x08':  # backspace
         return "Back"
     else:
         return key.decode()
@@ -18,6 +16,8 @@ class Grid:
         self.word=word
         self.nb_try=6
         self.current_try=0
+        self.WIN=False
+        self.LOSE=False
         self.line=[]
         self.grid=[]
         for _ in range(self.nb_try):
@@ -34,25 +34,76 @@ class Grid:
             print('\n_______________')
 
     def validated_word(self):
+        end = self.test_end()
+        if end in [-1,1]:
+            return end
         self.current_try+=1
-        self.grid[0][self.current_try]=self.word[0]
+        self.grid[self.current_try][0]=self.word[0]
+        return 0 # The game continue
 
 
-    def write(letter,position):
+    def test_end(self):
+        '''
+        0 -> The game is not finished\n
+        -1 -> Defeat\n
+        1  -> Victory
+        '''
+        condition = True
+        for i in range(len(self.word)):
+            if self.grid[self.current_try][i]=='.':
+                return 0
+            if self.grid[self.current_try][i]!=self.word[i]:
+                condition = False
+
+        if condition :
+            self.WIN = True
+            return 1
+        if self.current_try==self.nb_try-1:
+            self.LOSE = True
+            return -1
+        else :
+            return 0
+
+
+    def write(self,letter,position):
+        '''
+        return the new position of the letter in the word
+        -1 = erreur
+        '''
         if(letter=='Enter'):
             if(position==len(self.word)-1):
-                validated_word()
-                return position
+                self.validated_word()
+                return 1
             else :
-                return -1
+                return -1 #erreur
         elif(letter=='Back'):
-            self.grid[position-1][self.current_try]='.'
+            if position==1:
+                return -1
+            self.grid[self.current_try][position-1]='.'
             return position-1
-        elif(letter in ALPHABET):
-            self.grid[position][self.current_try]=letter
-            return position
+        elif(letter.upper() in ALPHABET):
+            if(position==len(self.word)-1):
+                return -1
+            self.grid[self.current_try][position]=letter.upper()
+            return position+1
         return -1
+    
+    
+    def jeu(self):
+        position=1
+        while not (self.WIN or self.LOSE):
+            self.display()
+            letter=get_key()
+            position=self.write(letter,position)
+            while position==-1:
+                letter=get_key()
+                position=self.write(letter,position)
+        if self.WIN :
+             print("win")
+        else:
+            print("lose")
+
 
             
-jeu=Grid("blabla")
-jeu.display()
+jeu=Grid("BLABLA")
+jeu.jeu()
